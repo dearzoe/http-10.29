dc69a247c3bc2464c2599b6b4653056a3b1f44a8
dearzoe <2421113739@qq.com>
2016-10-29 20:16:36 +0800

    引入mime

--- 2.server.js
+++ 2.server.js
@@ -1,9 +1,18 @@
 var http = require("http");
-var fs   =require("fs");
+var fs   = require("fs");
+var path = require("path");
+var mime = require("mime");
+/*var mime={
+    ".css":"text/css",
+    ".js":"text/javascript",
+    ".jpg":"image/jpg",
+    ".html":"text/html"
+};*/
 //创建一个http服务器；
 var server = http.createServer(function (req, res) {
     //设置响应类型到响应头中
-    res.setHeader('Content-Type','text/html;charset=utf-8');
+    //res.setHeader('Content-Type',mime[path.extname(req.url)]+';charset=utf-8');
+    res.setHeader('Content-Type',mime.lookup(req.url)+';charset=utf-8');
     if(req.url == "/clock"){
         res.end(new Date().toLocaleString());
     }else{
@@ -14,6 +23,6 @@ var server = http.createServer(function (req, res) {
     }
 });
 //创建一个监听
-server.listen(8899, function () {
+server.listen(8080, function () {
     console.log("监听中")
 });
